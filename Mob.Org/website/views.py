66f6bbf9538a4f444acf8b3bdcb56760dafef283
John Mac Molin Millares <97694486+gbox29@users.noreply.github.com>
2022-05-07 11:00:38 +0800

    Add files via upload

---
+++ Mob.Org/website/views.py
@@ -0,0 +1,73 @@
+from flask import Blueprint, redirect, render_template, request,url_for
+views = Blueprint('views',__name__)
+from . import mysql
+
+@views.route('/')
+def home():
+    return render_template("base.html")
+@views.route('/add_film', methods=['GET','POST'])
+def add_film():
+    cur = mysql.connection.cursor()
+    if request.method == 'POST':
+        name = request.form['item_name']
+        type = "film"
+        episode = request.form['episode']
+        date = request.form['date']
+        source = request.form['source']
+        demographic = request.form['demographic']
+        duration = request.form['duration']
+        poster = request.form['poster']
+        trailer = request.form['trailer']
+        cur.execute(""" INSERT INTO 
+                    t_item (item_name,t_type,episode,date_release,item_source,demographic,duration,poster,trailer) 
+                    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s) """,(name,type,episode,date,source,demographic,duration,poster,trailer))
+        mysql.connection.commit()
+        return redirect(url_for("views.film_table"))
+    return render_template("add_film.html")
+
+@views.route('/film_table')
+def film_table():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("Select * FROM t_item")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template("table_film.html",userDetails=userDetails)
+    return render_template("table_film.html")
+
+@views.route('/update_film/<string:id_data>', methods=['GET','POST'])
+def update_film(id_data):
+   cur = mysql.connection.cursor()
+   resultValue = cur.execute("Select * FROM t_item WHERE id = %s",(id_data))
+   if resultValue > 0:
+        userDetails = cur.fetchall()
+        if request.method == 'POST':
+            name = request.form['item_name']
+            episode = request.form['episode']
+            date = request.form['date']
+            source = request.form['source']
+            demographic = request.form['demographic']
+            duration = request.form['duration']
+            poster = request.form['poster']
+            trailer = request.form['trailer']
+            cur.execute(""" UPDATE t_item SET item_name=%s, episode=%s,date_release=%s,item_source=%s,
+                        demographic=%s,duration=%s,poster=%s,trailer=%s""",
+                        (name,episode,date,source,demographic,duration,poster,trailer))
+            mysql.connection.commit()
+            return redirect(url_for("views.film_table"))
+        return render_template("update_film.html",userDetails=userDetails)
+   return render_template("update_film.html")
+
+@views.route('/delete_film/<string:id_data>')
+def delete_film(id_data):
+    cur = mysql.connection.cursor()
+    cur.execute(""" DELETE FROM t_item WHERE id=%s""",(id_data))
+    cur.execute("ALTER TABLE t_item AUTO_INCREMENT = 1")
+    mysql.connection.commit()
+    return redirect(url_for("views.film_table"))
+
+
+
+
+
+
+
