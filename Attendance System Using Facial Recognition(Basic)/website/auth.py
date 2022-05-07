d915fca81a937bb68fcba8c9ee074a74e2cbec2c
John Mac Molin Millares <97694486+gbox29@users.noreply.github.com>
2022-05-07 10:59:31 +0800

    Delete Attendance System Using Facial Recognition(Basic) directory

--- Attendance System Using Facial Recognition(Basic)/website/auth.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, request,flash,redirect,url_for,session
-from . import mysql
-auth = Blueprint('auth',__name__)
-
-@auth.route('/login', methods = ['GET','POST'])
-def login():
-    pass
--- Attendance System Using Facial Recognition(Basic)/website/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, redirect, render_template,request,session,url_for,flash
-from . import mysql
-views = Blueprint('views',__name__)
-
-@views.route('/')
-def home():
-    return "hello views"
